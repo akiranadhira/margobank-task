beafa60787e09ff817ac0d598b69125be290485f
Akira Nadhira <akiranadhira1812@gmail.com>
2023-11-03 12:12:02 +0700

    create eureka server module for base microservices

---
+++ eureka/src/test/java/com/margobank/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margobank.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
